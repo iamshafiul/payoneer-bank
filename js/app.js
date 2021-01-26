6e45e9ec68601f265dd3d61fb52cad85ef21c947
Shafiul Alam <shafiul.primitivesolution@gmail.com>
2021-01-26 16:45:37 +0600

    complate website

--- js/app.js
+++ js/app.js
@@ -1,23 +1,44 @@
 // login button event handler
 
-let loginBtn = document.getElementById('login-id');
+const loginBtn = document.getElementById('login-id');
 loginBtn.addEventListener("click", function() {
-    let loginArea = document.getElementById('login_area');
+    const loginArea = document.getElementById('login_area');
     loginArea.style.display = 'none';
-    let transactionArea = document.getElementById('transaction_area');
+    const transactionArea = document.getElementById('transaction_area');
     transactionArea.style.display = 'block';
 })
 
 
-// deposit button deposit event handler
+// deposit button event handler
 let depositBtn = document.getElementById('deposit_button');
 depositBtn.addEventListener('click', function() {
-    let depositAmount = document.getElementById('deposit_amount').value;
-    let depositNumber = parseFloat(depositAmount);
+    const depositAmount = document.getElementById('deposit_amount').value;
+    const depositNumber = parseFloat(depositAmount);
 
-    let currentDeposit = document.getElementById('current_deposit').innerText;
-    let currentDepositNumber = parseFloat(currentDeposit);
 
-    const total = depositNumber + currentDeposit
-    document.getElementById('current_deposit').innerText = total;
+    spanUpdateText('current_balance', depositNumber);
+    spanUpdateText('current_deposit', depositNumber);
+
+
+
+    document.getElementById('deposit_amount').value = " ";
 });
+
+
+// withdraw button event handler 
+const withdrawBtn = document.getElementById('withdraw');
+withdrawBtn.addEventListener('click', function() {
+    const withdrawAmount = document.getElementById('withdraw_amount').value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+    spanUpdateText('current-withdraw', withdrawNumber);
+    spanUpdateText('current_balance', -1 * withdrawNumber);
+    document.getElementById('withdraw_amount').value = " ";
+});
+
+// update function 
+const spanUpdateText = (id, depositNumber) => {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
